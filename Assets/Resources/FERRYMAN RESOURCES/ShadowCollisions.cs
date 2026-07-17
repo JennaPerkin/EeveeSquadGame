9cfa2d4a8fa9906d36521deed10579d65ef0417c
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2026-07-17 15:29:24 -0400

    enemy death counter restrictions

--- Assets/Resources/FERRYMAN RESOURCES/ShadowCollisions.cs
+++ Assets/Resources/FERRYMAN RESOURCES/ShadowCollisions.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class ShadowCollisions : MonoBehaviour
 {
     private ProgressionVariables progressionCheck;
+    private Text enemiesKilledText;
     void Start()
     {
         progressionCheck = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ProgressionVariables>();
+        enemiesKilledText = GameObject.FindGameObjectWithTag("EnemyKilledUI").GetComponent<Text>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -14,11 +17,17 @@ public class ShadowCollisions : MonoBehaviour
         Debug.Log("Shadow Hit Something");
         if(collision.gameObject.CompareTag("Shadow"))
         {
-            //Debug.Log("Hit Shadow");
-            Destroy(collision.gameObject);
-            transform.localScale = new Vector3(transform.localScale.x + 0.5f, transform.localScale.y + 0.5f, transform.localScale.z);
-            progressionCheck.enemiesDefeated += 1;
-            Debug.Log("Enemies Defeated: " + progressionCheck.enemiesDefeated);
+            if(collision != null)
+            {
+                collision.GetComponent<EnemyDeath>().Death();
+                collision.GetComponent<BoxCollider2D>().enabled = false;
+                //Debug.Log("Hit Shadow");
+                //Destroy(collision.gameObject);
+                transform.localScale = new Vector3(transform.localScale.x + 0.5f, transform.localScale.y + 0.5f, transform.localScale.z);
+                //progressionCheck.enemiesDefeated += 1;
+                //enemiesKilledText.text = progressionCheck.enemiesDefeated.ToString();
+                //Debug.Log("Enemies Defeated: " + progressionCheck.enemiesDefeated);
+            }
         }
 
         else if(collision.gameObject.CompareTag("DestroyShadow"))
