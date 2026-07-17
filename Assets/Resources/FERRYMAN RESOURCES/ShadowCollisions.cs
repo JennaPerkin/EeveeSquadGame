8824bb5e38fce38a8c45870fd22084d8db6de048
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2026-07-17 09:50:36 -0400

    UI and Debug Log

--- Assets/Resources/FERRYMAN RESOURCES/ShadowCollisions.cs
+++ Assets/Resources/FERRYMAN RESOURCES/ShadowCollisions.cs
@@ -14,7 +14,7 @@ public class ShadowCollisions : MonoBehaviour
         Debug.Log("Shadow Hit Something");
         if(collision.gameObject.CompareTag("Shadow"))
         {
-            Debug.Log("Hit Shadow");
+            //Debug.Log("Hit Shadow");
             Destroy(collision.gameObject);
             transform.localScale = new Vector3(transform.localScale.x + 0.5f, transform.localScale.y + 0.5f, transform.localScale.z);
             progressionCheck.enemiesDefeated += 1;
@@ -23,7 +23,7 @@ public class ShadowCollisions : MonoBehaviour
 
         else if(collision.gameObject.CompareTag("DestroyShadow"))
         {
-            Debug.Log("Shadow Hit Ground");
+            //Debug.Log("Shadow Hit Ground");
             Destroy(gameObject);
         }
     }
