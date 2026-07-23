a6a275252441d57eb25b36adaa048e2f1cebd48f
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2026-07-23 17:39:06 -0400

    organized

--- Assets/FIND NAME HERE/Nyla/guard..cs
+++ Assets/FIND NAME HERE/Nyla/guard..cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class guard. : MonoBehaviour
+public class guard : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
